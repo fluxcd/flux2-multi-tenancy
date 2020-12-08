37a52dacc4050a4c81fa2a732e5a87873edf74ca
Stefan Prodan <stefan.prodan@gmail.com>
2020-12-08 11:38:22 +0200

    Automate Kyverno manifest updates
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- .github/actions/tools/entrypoint.sh
+++ .github/actions/tools/entrypoint.sh
@@ -7,23 +7,21 @@ KUSTOMIZE_VERSION="3.8.6"
 KUBEVAL_VERSION="0.15.0"
 
 mkdir -p $GITHUB_WORKSPACE/bin
+cd $GITHUB_WORKSPACE/bin
 
 curl -sL https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -o yq
 
-cp ./yq $GITHUB_WORKSPACE/bin
 chmod +x $GITHUB_WORKSPACE/bin/yq
 
 kustomize_url=https://github.com/kubernetes-sigs/kustomize/releases/download && \
 curl -sL ${kustomize_url}/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz | \
 tar xz
 
-cp ./kustomize $GITHUB_WORKSPACE/bin
 chmod +x $GITHUB_WORKSPACE/bin/kustomize
 
 curl -sL https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz | \
 tar xz
 
-cp ./kubeval $GITHUB_WORKSPACE/bin
 chmod +x $GITHUB_WORKSPACE/bin/kubeval
 
 echo "$GITHUB_WORKSPACE/bin" >> $GITHUB_PATH
